de075279a1e860d31b1ea9935413c2a0464b215c
Yonghoon <yonghoonj98@gmail.com>
2022-02-26 20:32:27 +0900

    유닛 테스트 코드 작성 완료

--- nest-2/src/movies/movies.service.spec.ts
+++ nest-2/src/movies/movies.service.spec.ts
@@ -14,6 +14,10 @@ describe('MoviesService', () => {
 
     service = module.get<MoviesService>(MoviesService);
   });
+  /* 
+  beforeEach 말고도 afterEach, afterAll, beforeAll이 있다.
+  예시로 afterAll() 안에는 데이터 베이스를 깨끗하게 모두 지워주는 함수를 넣을 수 있다.
+  */
 
   it('should be defined', () => {
     expect(service).toBeDefined();
@@ -85,7 +89,37 @@ describe('MoviesService', () => {
       expect(afterCreate).toBeGreaterThan(beforeCreate);
     });
   });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'test movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      service.update(1, { title: 'update test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('update test');
+    });
+
+    it('should throw NotFoundException', () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
 });
+
+/*
+이 코드는 테스트 전에 실행되는 함수에서 작성해도 된다. 
+service.create({
+  title: 'test movie',
+  genres: ['test'],
+  year: 2000,
+});
+*/
 
 // // 테스트 예시
 // it('should be 4', () => {
