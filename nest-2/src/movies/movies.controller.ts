2a1aba885c1a5d93858f319fa4f5de5ebcb51d0f
Yonghoon <yonghoonj98@gmail.com>
2022-02-26 17:03:57 +0900

    movies service, entities 작성

--- nest-2/src/movies/movies.controller.ts
+++ nest-2/src/movies/movies.controller.ts
@@ -9,33 +9,37 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   // search가 /:id 요청보다 아래 있으면 /:id 요청이 실행되니 유의해야 함
-  @Get('search')
-  search(@Query('year') searchYear: string) {
-    return `we are search movie ${searchYear}`;
-  }
+  // @Get('search')
+  // search(@Query('year') searchYear: string) {
+  //   return `we are search movie ${searchYear}`;
+  // }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string) {
-    return `this id = ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return `delete movie id = ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   // Put은 모든 리소스를 업데이트해주는데 Patch는 일부 리소스만 업데이트를 해줌
