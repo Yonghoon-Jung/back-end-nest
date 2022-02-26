b953ea095cd0ad01e70276d8da5e0c2f556303d8
Yonghoon <yonghoonj98@gmail.com>
2022-02-26 21:05:19 +0900

    e2e /movies/:id get 테스트 코드 작성 및 transform 적용

--- nest-2/test/app.e2e-spec.ts
+++ nest-2/test/app.e2e-spec.ts
@@ -1,17 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  // 테스트를 할 때마다 어플리케이션을 만들고 싶지 않으면 beforeEach -> beforeAll로 변경
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    // e2e 테스트에서는 main.ts에서 적용했던 transform이 적용되지 않아 에러가 뜸.
+    // 항상 이 점을 인지하고 e2e 또는 유닛 테스트를 수행해야 함
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -42,5 +52,17 @@ describe('AppController (e2e)', () => {
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
+  });
+
+  describe('/movies/:id', () => {
+    // nestjs에서 it.todo라는 기능을 제공한다. 해야 할 일을 메모?하는 기능
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it.todo('DELETE');
+    it.todo('PATCH');
   });
 });
