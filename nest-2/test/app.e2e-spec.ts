5884ebee0894a8a30dea775724093ab2aaa3d6d2
Yonghoon <yonghoonj98@gmail.com>
2022-02-26 20:49:28 +0900

    e2e movies get, post, delete 테스트 코드 작성

--- nest-2/test/app.e2e-spec.ts
+++ nest-2/test/app.e2e-spec.ts
@@ -16,9 +16,31 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect('welcome');
+  });
+
+  // 테스팅 데이터베이스와 메인 데이터베이스를 나눠서 가지고 있어야 함.
+  describe('/movies', () => {
+    it('GET', () => {
+      // request는 슈퍼테스트에서 가져와서 쓰는 것이고,
+      // getHttpServer는 localhost:3000 처럼 작성하지 않으려고 쓰는 것임
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'test',
+          genres: ['test'],
+          year: 2000,
+        })
+        .expect(201);
+    });
+
+    // delete는 method not allowed가 아닌 not found 404를 받는다.
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
   });
 });
