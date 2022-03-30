d81d2b77adb35f51eb8f06dd23a6f796f75ef550
Yonghoon <yonghoonj98@gmail.com>
2022-03-30 14:48:35 +0900

    WS 공부

--- nest-3/src/app.module.ts
+++ nest-3/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common';
+import { WebsocketGateway } from './websocket.gateway';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
+  providers: [WebsocketGateway],
 })
 export class AppModule {}
--- nest-3/src/main.ts
+++ nest-3/src/main.ts
@@ -2,12 +2,13 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server');
   const port = serverConfig.port;
-
+  app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(port);
   Logger.log(`Application running on port ${port}`);
 }
