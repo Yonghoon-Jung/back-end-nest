93b810fc8999094ffd9147dbeb3cb81a6dfb7c91
Yonghoon <yonghoonj98@gmail.com>
2022-05-12 17:41:04 +0900

    express middleware 이해하기 수강

--- inflearn/src/app.ts
+++ inflearn/src/app.ts
@@ -4,8 +4,30 @@ import { Cat, CatType } from "./app.model";
 const app: express.Express = express();
 const port: number = 8000;
 
+app.use((req, res, next: express.NextFunction) => {
+  console.log(req.rawHeaders[1]);
+  next();
+});
+
+app.get("/cats/som", (req, res, next) => {
+  console.log("som middleware");
+});
+
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send({ cats: Cat });
+});
+
+app.get("/cats/blue", (req, res) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get("/cats/som", (req, res) => {
+  res.send({ som: Cat[1] });
+});
+
+// 경로를 찾지 못했을 경우 에러 핸들링 미들웨어
+app.use((req, res, next: express.NextFunction) => {
+  res.send({ error: "404 not found error" });
 });
 
 app.listen(port, () => {
