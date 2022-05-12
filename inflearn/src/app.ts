d59a163272944fe296b61b6fa6e9834a95da7aa4
Yonghoon <yonghoonj98@gmail.com>
2022-05-12 17:27:12 +0900

    고양이 데이터 모킹하기

--- inflearn/src/app.ts
+++ inflearn/src/app.ts
@@ -1,9 +1,11 @@
 import * as express from "express";
+import { Cat, CatType } from "./app.model";
+
 const app: express.Express = express();
 const port: number = 8000;
 
 app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("Hello World!");
+  res.send({ cats: Cat });
 });
 
 app.listen(port, () => {
