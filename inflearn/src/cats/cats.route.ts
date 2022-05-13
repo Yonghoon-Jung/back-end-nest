bc275ff8de5094163eb5c285e6e0f3aeef8dc3da
Yonghoon <yonghoonj98@gmail.com>
2022-05-13 09:15:35 +0900

    고양이 데이터 Update Delete API 개발

--- inflearn/src/cats/cats.route.ts
+++ inflearn/src/cats/cats.route.ts
@@ -63,4 +63,78 @@ router.post("/cats", (req, res) => {
   }
 });
 
+// PUT 고양이 정보 전체 수정
+router.put("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = body;
+        result = cat;
+      }
+    });
+
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+// PATCH 고양이 정보 일부 수정
+router.patch("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = { ...cat, ...body };
+        result = cat;
+      }
+    });
+
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+// DELETE 고양이 정보 삭제
+router.delete("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const newCat = Cat.filter((cat) => cat.id !== params.id);
+
+    res.status(200).send({
+      success: true,
+      data: newCat,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
 export default router;
