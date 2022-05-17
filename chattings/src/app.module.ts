f522a33e46da16f9b828bd14a204bc884cd058f8
Yonghoon <yonghoonj98@gmail.com>
2022-05-17 14:17:44 +0900

    MVC 패턴, 프로젝트 셋업

---
+++ chattings/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
