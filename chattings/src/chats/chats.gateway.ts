a1b18dac37ddf6bbe6e1ea179e61aa85fc60b263
Yonghoon <yonghoonj98@gmail.com>
2022-05-17 15:57:49 +0900

    유니캐스팅 (Unicasting) : emit & on

--- chattings/src/chats/chats.gateway.ts
+++ chattings/src/chats/chats.gateway.ts
@@ -1,9 +1,20 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'dgram';
 
 @WebSocketGateway()
 export class ChatsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
+  @SubscribeMessage('new_user') // new_user -> 이벤트 이름
+  handleNewUser(
+    @MessageBody() username: string,
+    @ConnectedSocket() socket: Socket,
+  ): string {
+    console.log(username);
+    socket.emit('hello_user', 'hello' + username);
     return 'Hello world!';
   }
 }
