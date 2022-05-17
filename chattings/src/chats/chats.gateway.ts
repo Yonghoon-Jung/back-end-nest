38c16d984720f179133f7770289607ebc417871a
Yonghoon <yonghoonj98@gmail.com>
2022-05-17 16:17:04 +0900

    네임스페이스의 이해와 Gateway 생명주기

--- chattings/src/chats/chats.gateway.ts
+++ chattings/src/chats/chats.gateway.ts
@@ -1,13 +1,39 @@
+import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
-import { Socket } from 'dgram';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  // 클라이언트와 연결이 끊기자마자 실행되는 함수
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  // 클라이언트와 연결되자마자 실행되는 함수
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  // constructor 다음으로 실행되는 거
+  afterInit(server: any) {
+    this.logger.log('init');
+  }
   @SubscribeMessage('new_user') // new_user -> 이벤트 이름
   handleNewUser(
     @MessageBody() username: string,
