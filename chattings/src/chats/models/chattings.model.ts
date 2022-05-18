6d162441fdbcb2a003827a24db7c965a32d57276
Yonghoon <yonghoonj98@gmail.com>
2022-05-18 11:10:39 +0900

    DB 설계

---
+++ chattings/src/chats/models/chattings.model.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+import { IsString, IsNotEmpty } from 'class-validator';
+import { Socket as SocketModel } from './sockets.model';
+
+const options: SchemaOptions = {
+  collection: 'chattings',
+  timestamps: true,
+};
+
+@Schema(options)
+export class Chatting extends Document {
+  @Prop({
+    type: {
+      _id: { type: Types.ObjectId, required: true, ref: 'sockets' },
+      id: { type: String },
+      username: { type: String, required: true },
+    },
+  })
+  @IsString()
+  @IsNotEmpty()
+  user: SocketModel;
+
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  chat: string;
+}
+
+export const ChattingSchema = SchemaFactory.createForClass(Chatting);
---
+++ chattings/src/chats/models/sockets.model.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { IsString, IsNotEmpty } from 'class-validator';
+
+const options: SchemaOptions = {
+  id: false, // id가 true일 경우 ObjectId와 동일하게 생성되지만 소켓 id를 넣어줄 것이기에 false
+  collection: 'chattings',
+  timestamps: true,
+};
+
+@Schema(options)
+export class Socket extends Document {
+  @Prop({
+    unique: true,
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+}
+
+export const SocketSchema = SchemaFactory.createForClass(Socket);
